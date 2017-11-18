6732a4e475dea10581c8d6037554bb49a6da0903
Marfuen <marfuen98@gmail.com>
2017-11-18 10:45:27 -0500

    Changed the json file

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
