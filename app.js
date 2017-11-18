7c2c606395c99dd9f40b2705367409ccf0ac95d9
Marfuen <marfuen98@gmail.com>
2017-11-18 10:58:31 -0500

    i

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
