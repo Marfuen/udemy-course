e64f87c23df1bc639463720eee6e925ac05c8615
Marfuen <marfuen98@gmail.com>
2017-11-18 12:05:20 -0500

    i

--- app/app.js
+++ app/app.js
@@ -1,2 +1,15 @@
 var express = require("express"); 
 var app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get("/", function(req,res){
+    res.render("landing");
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
